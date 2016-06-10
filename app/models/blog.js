fe644e944d77201747d90b118f9a906fe5f15d93
CodeHz <codehz@outlook.com>
2016-06-10 03:42:46 +0000

    add blog model

---
+++ app/models/blog.js
@@ -0,0 +1,25 @@
+import mongoose, {Schema} from 'mongoose'
+import Promise from 'bluebird'
+import uniqueValidator from 'mongoose-unique-validator'
+import timestamp from 'mongoose-timestamp'
+
+const blogSchema = new Schema({
+  blogname: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
+blogSchema.plugin(uniqueValidator)
+blogSchema.plugin(timestamp)
+
+const model = mongoose.model('Blog', blogSchema)
+Promise.promisifyAll(model)
+Promise.promisifyAll(model.prototype)
+
+export default model
